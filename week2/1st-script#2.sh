dc982721e9988949a80a38b74781e593e6eb1a43
Burhan <43483741+burhankhuzam@users.noreply.github.com>
2018-10-15 20:01:09 +0200

    Add files via upload
    CLI Home Work #2

---
+++ week2/1st-script#2.sh
@@ -0,0 +1,72 @@
+Last login: Mon Oct 15 09:45:48 on console
+Burhans-iMac:~ burhanmaster$ ls
+Applications		Downloads		Music
+Creative Cloud Files	Dropbox			Pictures
+Desktop			Library			Public
+Documents		Movies			TomTom Sports
+Burhans-iMac:~ burhanmaster$ cd desktop
+Burhans-iMac:desktop burhanmaster$ ls
+1st-script.sh
+Anghami
+Anghami Fairouz
+Balsamiq Mockups 3.app
+Burhan Khuzam CV_2018-EN.pdf
+Burhan Khuzam CV_2018-NL.pdf
+Burhan Khuzam Portfolio Dubai.pdf
+Burhan Khuzam Portfolio NL.pdf
+CSS3-Cheat-Sheet.pdf
+Dima Olbeh CV 2018.pdf
+Eddy Sticker 2018.ai
+Emmet-Exercise-Files.zip
+HTML-Cheat-Sheet
+Logopedie 2nd form.ai
+Mel7em Barakat
+Motivatiebrief.pdf
+Screen Shot 2018-09-24 at 10.11.10 AM.png
+Screen Shot 2018-10-15 at 3.43.59 PM.png
+Screen Shot 2018-10-15 at 6.29.06 PM.png
+Screen Shot 2018-10-15 at 7.01.13 PM.png
+Think with Google
+Visual Studio Code.app
+resources.pdf
+veve.html
+veve_files
+Burhans-iMac:desktop burhanmaster$ nano 1st-script.sh
+
+
+  GNU nano 2.0.6             File: 1st-script.sh                      Modified  
+
+mkdir W3-CLI
+cd W3-CLI
+touch blank
+
+echo "Hallo" > greetings.txt
+echo "Hallo" >> greetings.txt
+echo "Hallo" >> greetings.txt
+echo "Hallo" >> greetings.txt
+echo "Hallo" >> greetings.txt
+
+cp greetings.txt 1.txt
+cp greetings.txt 2.txt
+cp greetings.txt 3.txt
+cp greetings.txt 4.txt
+cp greetings.txt 5.txt
+
+echo "cat" > pets.txt
+echo "Dog" >> pets.txt
+echo "Hamster" >> pets.txt
+
+echo "cat" > commands.txt
+echo "ls" >> commands.txt                      
+echo "pwd" >> commands.txt
+
+sort -u commands.txt pets.txt > lovelyCommands.txt
+
+
+
+
+
+
+
+^G Get Help  ^O WriteOut  ^R Read File ^Y Prev Page ^K Cut Text  ^C Cur Pos
+^X Exit      ^J Justify   ^W Where Is  ^V Next Page ^U UnCut Text^T To Spell
